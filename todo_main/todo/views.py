1bc62a749043fa70e9146c61c9fdd26c234ecb6d
K1R1DA <duqueaguilarkiranantoniog23@cbtis116.edu.mx>
2025-04-03 16:10:44 -0700

    Se modificaron varios errores en el is_complete y en el templete home.htmls

--- todo_main/todo/views.py
+++ todo_main/todo/views.py
@@ -1,48 +1,46 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Task
 # Create your views here.
-
-def home(self):
-    task = Task.objects.filter(is_complete=False).order_by('-update_at')
-    complete_task = Task.objects.filter(is_complete=True)
+def home(request):
+    tasks = Task.objects.filter(is_completed=False).order_by('-update_at')
+    completed_tasks = Task.objects.filter(is_completed=True)
 
     context = {
-        'task' : task,
-        'complete_task': complete_task
+        'tasks': tasks,
+        'completed_tasks': completed_tasks
     }
-    return render(self, 'home.html')
+    return render(request, 'home.html', context)
 
 def add_task(request):
     print(request.POST['task'])
-    task=request.POST['task']
+    task = request.POST['task']
     Task.objects.create(task=task)
 
     return redirect('home')
 
 def task_done(request, id):
-    task=get_object_or_404(Task, id=id)
-    task_is_complete=True
+    task = get_object_or_404(Task, id=id)
+    task.is_completed = True
     task.save()
 
     return redirect('home')
 
 def task_undone(request, id):
-    task=get_object_or_404(Task, id=id)
-    task_is_complete=False
+    task = get_object_or_404(Task, id=id)
+    task.is_completed = False
     task.save()
 
     return redirect('home')
 
 def edit_task(request, id):
-    get_task=get_object_or_404(Task, id=id)
+    get_task = get_object_or_404(Task, id=id)
     if request.method == 'POST':
-        new_task=request.POST['task']
-        get_task.task=new_task
+        new_task = request.POST['task']
+        get_task.task = new_task
         get_task.save()
         return redirect('home')
-    
     else:
         context = {
-            'get_task' : get_task,
+            'get_task': get_task,
         }
         return render(request, 'edit_task.html', context)
